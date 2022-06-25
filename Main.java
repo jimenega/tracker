24659fab7672400fb6ed3131a513d50d1f881a99
jimenega <jimenega@users.noreply.github.com>
2022-06-25 17:39:13 -0600

    Removed todo note in Main

--- Main.java
+++ Main.java
@@ -8,9 +8,3 @@ public class Main {
         tracker.Console();
     }
 }
-
-//todo: You can edit a language injection in your code using a dedicated editor.
-//  For example, to edit a regular expression, start typing it, press Alt+Enter and choose Edit RegExp Fragment.
-//  The regular expression opens in a separate tab in the editor, where you can type backslashes as is.
-//  All changes are synchronized with the original regular expression, and escape characters are presented automatically.
-//  When ready, press Escape to close the regular expression editor.
