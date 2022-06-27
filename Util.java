b776ce14f7dec5dcd8a108dd74f7a9283ab7fab6
jimenega <jimenega@users.noreply.github.com>
2022-06-27 10:17:12 -0600

    Combined methods in class Util

--- Util.java
+++ Util.java
@@ -3,27 +3,12 @@ package tracker;
 import java.util.List;
 public final class Util {
 
-    // Find Command in Command.RESERVED - look for test in RESERVED
-    public static boolean contains(String test) {
+    static boolean isCommand(String input) {
         for (Command.RESERVED c : Command.RESERVED.values()) {
-            if (c.name().equals(test)) {
-                return true;
-            }
+            if (c.name().equals(input.toLowerCase())) return true;
         }
         return false;
     }
-
-    static boolean isCommand(String input) {
-        if (contains(input.toLowerCase()))
-            return true;
-        else return false;
-    }
-
-    /*static Command.RESERVED isCommand(String input) {
-        if (contains(input.toLowerCase()))
-            return Command.RESERVED.valueOf(input.toLowerCase());
-        else return null;
-    }*/
 
     static boolean inputChecks(List<String> input) {
         if (input.get(0).isBlank()) {
@@ -32,44 +17,40 @@ public final class Util {
         }
         else return false;
     }
-
+//todo: incorporate isCommand into errorChecks ???
     static boolean errorChecks(List<String> input) {
-        if (!isCommand(input.get(0))) {
+        if (isCommand(input.get(0))) {
+            String command = input.get(0).toLowerCase();
+            switch (command) {
+                case "exit" :
+                    Interface.commandLevel = 0;
+                    Interface.activeCommand = Command.RESERVED.exit;
+                    new Exit().execute();
+                    break;
+                case "back" :
+                    Interface.commandLevel = 0;
+                    Interface.activeCommand = Command.RESERVED.back;
+                    new Back().execute();
+                    break;
+                case "add" :
+                    Interface.commandLevel = 0;
+                    Interface.activeCommand = Command.RESERVED.back;
+                    new Add().execute();
+                    break;
+            }
+            return false;
+        }
+        else {
             Message.unknownCommand_M();
             return true;
         }
-        else return false;
     }
-//todo: maybe commandChecks could be combined in errorChecks ??
-    static boolean commandChecks(List<String> input) {
-        String command = input.get(0).toLowerCase();
-        switch (command) {
-            case "exit" :
-                Interface.commandLevel = 0;
-                Interface.activeCommand = Command.RESERVED.exit;
-                new Exit().execute();
-                break;
-            case "back" :
-                Interface.commandLevel = 0;
-                Interface.activeCommand = Command.RESERVED.back;
-                new Back().execute();
-                break;
-            case "add" :
-                Interface.commandLevel = 0;
-                Interface.activeCommand = Command.RESERVED.back;
-                new Add().execute();
-                break;
-            default:
-                return false;
-        }
-        return true;
-    }
-//todo: experiment with removing statusConsoleInput ??? maybe
+//todo: See is what methods can be moved to class Interface ???
     static void verifyConsoleInput(List<String> input) {
         int statusConsoleInput = 0;
         if(inputChecks(input)) {statusConsoleInput = -1;}
         else if (errorChecks(input)) {statusConsoleInput = -2;}
-        else if (commandChecks(input)) {statusConsoleInput = 1;}
+        //else if (commandChecks(input)) {statusConsoleInput = 1;}
         System.out.println("statusConsoleInput: " + statusConsoleInput);
     }
 }
