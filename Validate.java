4b05cbfcf4c9e4381cb2a52bd279d17a278354ef
jimenega <jimenega@users.noreply.github.com>
2022-07-22 17:48:59 -0600

    Added method assign() to clas Validate

--- Validate.java
+++ Validate.java
@@ -1,15 +1,30 @@
 package tracker;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Validate {
     List<String> rawInput;
+    //int lastStringIndex;
+
+    List<String> lname;
+    String fname;
+    String email;
 
     Validate(List<String> rawInput) {
         this.rawInput = rawInput;
-        printRawInput(this.rawInput);
+        assign();
     }
-    void printRawInput(List<String> rawInput) {
-        System.out.println(rawInput);
+        //todo:  check for entry size names > than 2
+    void assign(){
+        List<String> rawCopy = new ArrayList<>(this.rawInput);
+        int stringIndex = rawCopy.size() - 1;
+        fname = rawCopy.get(0);
+        email = rawCopy.get(stringIndex);
+        rawCopy.remove(0);
+        stringIndex = rawCopy.size() - 1;
+        rawCopy.remove(stringIndex);
+        lname = new ArrayList<>(rawCopy);
+        System.out.println("name: " + fname + " last name: " + lname + " email: " + email);
     }
 }
